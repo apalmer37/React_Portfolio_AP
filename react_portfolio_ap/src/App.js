a1010e2a58e295c94203796632d8c203188ef037
Alex Palmer <alexpalmer37@gmail.com>
2021-11-05 00:00:29 -0400

    containers setup and styled

--- react_portfolio_ap/src/App.js
+++ react_portfolio_ap/src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Work from "./components/work/Work";
+import Contact from "./components/contact/Contact";
+import "./app.css";
+
 function App() {
   return (
     <div className="app">
-      test script
+    <Navbar/>
+    <div className="container">
+    <Intro/>
+    <Portfolio/>
+    <Work/>
+    <Contact/>
+    </div>
     </div>
   );
 }
