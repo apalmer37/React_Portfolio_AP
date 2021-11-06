47f7f12920b62590fcf1fbc0666ce7c05696e8f5
Alex Palmer <alexpalmer37@gmail.com>
2021-11-05 23:17:33 -0400

    contact form created

--- react_portfolio_ap/src/components/intro/Intro.js
+++ react_portfolio_ap/src/components/intro/Intro.js
@@ -3,7 +3,11 @@ import "./intro.css"
 export default function Intro() {
     return(
         <div className="intro" id="intro">
-
+        
+        <img className="center-img" src="./Assets/AlexPalmer_img.jpeg" alt="Headshot 2019" />
+            <p className="about" id="about">
+            My name is Alex Palmer. I was born in Richmond, VA and I have lived here for my entire life. I am the manager of program business for Brandito. My initial goal with becomming a web developer is to help with my current role at Brandito. 
+            </p>
         </div>
     )
 }
--- react_portfolio_ap/src/components/work/Work.js
+++ react_portfolio_ap/src/components/work/Work.js
@@ -3,7 +3,43 @@ import "./work.css"
 export default function Work() {
     return(
         <div className="work" id="work">
+                <div className="worktitle" id="worktitle">
+                    <h1>My Work</h1>
+                </div>
 
+                <div className="container-work">
+                    <div className="card">
+                        <header>Crypto Tracker</header>
+                        <a href="https://apalmer37.github.io/Crypto_Tracker_Project/">
+                        <img src="./assets/crypto_tracker_SS.png" alt="crypto_tracker_project"/>
+                        </a>
+                    </div>
+                    <div className="card">
+                        <header>COVID-19 PPE Charity Store</header>
+                        <a href="https://charity-store.herokuapp.com/">
+                        <img src="./assets/COVID-19 PPE Charity Store.png" alt="PPE_Store"/>
+                        </a>
+                    </div>
+                    <div className="card">
+                        <header>Code Refactor</header>
+                        <a href="https://apalmer37.github.io/HW1-Apalmer/">
+                        <img src="./assets/hori_new_SS.png" alt="refactor"/>
+                        </a>
+                    </div>
+                    <div className="card">
+                        <header>Random Password Generator</header>
+                        <a href="https://apalmer37.github.io/Javascript_PasswordGenterator_AlexPalmer/">
+                        <img src="./assets/Password_Gen_SS.png" alt="password"/>
+                        </a>
+                    </div>
+                    <div className="card">
+                        <header>Work Day Scheduler</header>
+                        <a href="https://apalmer37.github.io/Work_Day_Scheduler/">
+                        <img src="./assets/work_day.png" alt="work_day"/>
+                        </a>
+                    </div>
+                </div>
+            
         </div>
     )
 }
